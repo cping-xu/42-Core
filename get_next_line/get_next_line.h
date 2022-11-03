797c9f7eb19c22ec0cf50a81b46ea192a806f3d2
Chia Ping Xuan <cping-xu@u81z06s02.42kl.edu.my>
2022-11-03 18:42:00 +0800

    2

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cping-xu <cping-xu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 22:50:57 by cping-xu          #+#    #+#             */
+/*   Updated: 2022/10/21 12:50:11 by cping-xu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*check_c(char *str, int sep);
+int		ft_strlen(char *str);
+
+#endif
